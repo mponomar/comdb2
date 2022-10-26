2ce23234aeeeb0b42556b9a69dce157ce6efda8b
Mike Ponomarenko <mponomarenko@bloomberg.net>
2022-10-26 09:27:14 -0400

    Start logbloat systable

--- sqlite/ext/comdb2/comdb2systblInt.h
+++ sqlite/ext/comdb2/comdb2systblInt.h
@@ -54,6 +54,7 @@ int systblFingerprintsInit(sqlite3 *);
 int systblViewsInit(sqlite3 *);
 int systblSQLClientStats(sqlite3 *);
 int systblSQLIndexStatsInit(sqlite3 *);
+int systblLogBloat(sqlite3 *);
 
 int comdb2_next_allowed_table(sqlite3_int64 *tabId);
 
